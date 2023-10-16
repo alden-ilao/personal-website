b85812fe22994209d4e87d6ceb2c997d3d72baa7
Alden Ilao <aldenilaodownloadthings@gmail.com>
2023-10-16 16:47:58 -0700

    header nav + tailwind.css integration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { links as tabItems } from "../lib/data";
+import { ReactNode, useState } from "react";
+import { motion } from "framer-motion";
+
+type TabProps = {
+  isSelected: boolean;
+  onSelect: (index: number) => void;
+  children?: ReactNode;
+};
+
+const Tab = ({ children, isSelected }: TabProps) => {
+  return <li>{children}</li>;
+};
 
 const Header = () => {
-  return <div>Header</div>;
+  const [activeTab, setActiveTab] = useState(0);
+
+  const onSelectHandler = (index: number) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <header className="relative z-[999]">
+      <motion.div
+        className="fixed left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+        initial={{ x: "-50%", y: 25, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      ></motion.div>
+      {/* <menu id="tabs">
+        {tabItems.map((tab, index) => {
+          return (
+            <Tab
+              key={tab.hash}
+              isSelected={activeTab === index}
+              onSelect={onSelectHandler.bind(index)}
+            >
+              {tab.name}
+            </Tab>
+          );
+        })}
+      </menu> */}
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,20 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import classes from "./RootLayout.module.css";
 import { FC } from "react";
 
 const RootLayout: FC = () => {
   return (
-    <>
-      <div className={`${classes["blur"]} ${classes["blur-left"]}`}></div>
-      <div className={`${classes["blur"]} ${classes["blur-right"]}`}></div>
+    <div className="relative">
+      <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
+      <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+
       <Header />
       <Outlet></Outlet>
       <Footer />
-    </>
+    </div>
   );
 };
 
-// <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 export default RootLayout;
