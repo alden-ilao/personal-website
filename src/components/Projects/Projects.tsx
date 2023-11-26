31e95fa5a1786be05d6938a4f89c3d8660b3f5bf
Alden Ilao <aldenilaodownloadthings@gmail.com>
2023-11-25 19:47:56 -0800

    added projects (next scroll behavior)

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -21,8 +21,8 @@ type ProjectProps = (typeof projectData)[number]; // uses the generic shape of p
 
 const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
   return (
-    <section className="group relative mx-auto mb-3 max-w-[42rem] overflow-hidden border border-black/5 last:mb-0 sm:mb-8 sm:h-[20rem] sm:pr-8">
-      <div className="flex h-full flex-col py-5 pt-4 sm:max-w-[50%] sm:pb-8 sm:pl-10 sm:pr-2">
+    <section className="group relative mx-auto mb-3 max-w-[42rem] overflow-hidden border border-black/5 bg-gray-100 transition last:mb-0 even:pl-8 hover:bg-gray-200 sm:mb-8 sm:h-[20rem] sm:pr-8">
+      <div className="flex h-full flex-col py-5 pt-4 sm:max-w-[50%] sm:pb-8 sm:pl-10 sm:pr-2 sm:group-even:ml-[20rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
         <ul className="mt-6 flex flex-wrap gap-2 sm:mt-auto">
@@ -39,7 +39,17 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
       <img
         src={imageUrl}
         alt="Project Image"
-        className="block sm:absolute sm:-right-40 sm:top-8 sm:block sm:w-[28.25rem] sm:rounded-t-lg sm:shadow-2xl"
+        className="block transition group-even:-left-40 group-even:right-[initial] 
+        sm:absolute 
+        sm:-right-40 
+        sm:top-8 
+        sm:block
+
+        sm:w-[28.25rem] 
+        sm:rounded-t-lg
+        sm:shadow-2xl
+
+        sm:group-hover:-translate-x-3 sm:group-hover:translate-y-3 sm:group-hover:-rotate-2 sm:group-hover:scale-105 sm:group-even:group-hover:translate-x-3 sm:group-even:group-hover:translate-y-3 sm:group-even:group-hover:rotate-2"
       />
     </section>
   );
